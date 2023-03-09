8429801d1d3509448be0ed40c4f0244d2c3dbe31
jung <102740808+Jung-eee@users.noreply.github.com>
2023-03-09 17:32:56 +0900

    dto 추가

---
+++ src/main/java/com/kspia/sscmservice/dto/SmCodeDto.java
@@ -0,0 +1,44 @@
+package com.kspia.sscmservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @since 2023. 03. 09
+ * @author jung
+ *
+ * @Discript
+ * ---------------------------------------------------
+ * 개요 : SmCodeDto 작성
+ * ---------------------------------------------------
+ * @EditHIstory
+ * 개정이력
+ * 2023-03.09 jung: 최초 작성
+ * 2023-03.09 jung : SmCodeDto 작성
+ */
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+public class SmCodeDto {
+
+	//코드
+	private String code;
+	//코드이름
+	private String codeName;
+	//코드 설명
+	private String description;
+	//상위 코드
+	private String parentCode;
+	//코드 레벨
+	private int level;
+	//표시 순서
+	private int displayOrder;
+	//사용여부
+	private Boolean useYn;
+	//부 코드
+	private String subCode;
+	//지자체 번호
+	private Long groupSid;
+
+}
---
+++ src/main/java/com/kspia/sscmservice/dto/SmUserDto.java
@@ -0,0 +1,48 @@
+package com.kspia.sscmservice.dto;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+/**
+ * @since 2023. 03. 09
+ * @author jung
+ *
+ * @Discript
+ * ---------------------------------------------------
+ * 개요 : SmUserDto작성
+ * ---------------------------------------------------
+ * @EditHIstory
+ * 개정이력
+ * 2023-03.09 jung: 최초 작성
+ * 2023-03.09 jung : SmUserDto작성
+ */
+
+@Data
+@NoArgsConstructor
+public class SmUserDto {
+
+	private String email;
+	private String name;
+	private String department;
+	private String departmentSubCode;
+	private String departmentName;
+	private String deptCode;
+	private String position;
+	private Boolean activated;
+	private Boolean accessWeb;
+	private Boolean accessMobile;
+	private List<String> assignedRoles;
+	private String latestUpdater;
+	private String ipAddress;
+	private Integer loginCount;
+	private Long groupSid;
+	private String geoX;
+	private String geoY;
+	private String zoom;
+	private String dashboardDefaultSelect;
+	private String timeLimitCd;
+	private String password;
+	private String passwordCompare;
+	private List<String> bookMarkList;
+
+}
